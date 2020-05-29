514b0a3cca597d5d8fdfc92ff3c94d9b89ba6899
aimlauer <suurrend3r@gmail.com>
2020-05-29 18:20:57 -0300

    changed favicon

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import GetCountries from './components/GetCountries';
 
-
 class App extends React.Component {
+
   render(){
     return (
     <div className="container">
